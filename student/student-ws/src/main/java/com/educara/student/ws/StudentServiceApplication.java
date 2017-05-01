11c4cbc0af4aff16ad7150109b23831990d2fd89
Kamlesh Kumar <kamlesh.kumar@educara.com>
2017-05-01 23:22:25 +0530

    Fixed build issue

--- student/student-ws/src/main/java/com/educara/student/ws/StudentServiceApplication.java
+++ student/student-ws/src/main/java/com/educara/student/ws/StudentServiceApplication.java
@@ -1,9 +1,12 @@
 package com.educara.student.ws;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StudentServiceApplication {
 	
 	public static void main(String[] args) {
-		
+		SpringApplication.run(StudentServiceApplication.class, args); 
 	}
-
 }
--- student/student-ws/src/main/java/com/educara/student/ws/controller/StudentServiceController.java
+++ student/student-ws/src/main/java/com/educara/student/ws/controller/StudentServiceController.java
@@ -1,7 +1,10 @@
 package com.educara.student.ws.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import com.educara.student.api.StudentService;
 
+@RestController
 public class StudentServiceController implements StudentService {
-
+	
 }
